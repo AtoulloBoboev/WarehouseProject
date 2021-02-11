7deedff804ceb4b0bd3cb4a92570653df6e252e9
Atoullo Boboev <atoullo.boboev@gmail.com>
2021-02-11 16:01:17 -0500

    interface (D11 T1)

---
+++ src/interfaces/Courier.java
@@ -0,0 +1,48 @@
+package interfaces;
+
+public class Courier implements Worker {
+
+    private int salary;
+    private static final int TASK_SALARY = 100;
+    private Warehouse w;
+    private boolean isPayed;
+
+    public Courier(Warehouse w) {
+        this.w = w;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public void doWork() {
+        salary+=TASK_SALARY;
+        w.incrementDeliveredOrders();
+
+    }
+
+    @Override
+    public void bonus() {
+        if (w.getCountDeliveredOrders()<10_000){
+            System.out.println("Бонус пока не доступен");
+            return;
+        }
+
+        if (isPayed){
+            System.out.println("Бонус уже был выплачен");
+            return;
+        }
+
+        salary+=50000;
+        isPayed =true;
+    }
+
+    @Override
+    public String toString() {
+        return "Courier{" +
+                "salary=" + salary +
+                ", isPayed=" + isPayed +
+                '}';
+    }
+}
---
+++ src/interfaces/Picker.java
@@ -0,0 +1,47 @@
+package interfaces;
+
+public class Picker implements Worker {
+    private int salary;
+    private static final int TASK_SALARY = 80;
+    private Warehouse w;
+    private boolean isPayed;
+
+    public Picker(Warehouse w) {
+        this.w = w;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public void doWork() {
+        salary+=TASK_SALARY;
+        w.incrementPickedOrders();
+
+    }
+
+    @Override
+    public void bonus() {
+        if (w.getCountPickedOrders()<10_000){
+            System.out.println("Бонус пока не доступен");
+        return;
+        }
+
+        if (isPayed){
+            System.out.println("Бонус уже был выплачен");
+            return;
+        }
+
+        salary+=70000;
+        isPayed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Picker{" +
+                "salary=" + salary +
+                ", isPayed=" + isPayed +
+                '}';
+    }
+}
